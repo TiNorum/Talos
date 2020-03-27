fa9706af94142735da559dd450b9853c2bd7a90e
danilov032 <danilov032@mail.ru>
2020-03-28 02:02:04 +0300

    add task 23(2)

--- app/src/main/java/com/example/myapplication/Instruments/Constants.java
+++ app/src/main/java/com/example/myapplication/Instruments/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     private Constants(){};
 
-    public static final String HOST = "194.87.234.126";
+    public static final String HOST = "193.87.234.126";
     public static final int PORT = 8080;
 }
