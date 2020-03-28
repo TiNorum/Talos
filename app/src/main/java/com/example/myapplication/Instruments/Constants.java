5826fa30d4a78318eea83431393bab320e5addfc
TiNorum <56567606+tinorum@users.noreply.github.com>
2020-03-28 12:39:00 +0300

    ;

--- app/src/main/java/com/example/myapplication/Instruments/Constants.java
+++ app/src/main/java/com/example/myapplication/Instruments/Constants.java
@@ -2,8 +2,14 @@ package com.example.myapplication.Instruments;
 
 public class Constants {
 
+
     private Constants(){};
 
-    public static final String HOST = "193.87.234.126";
+
+    public static final String NEXT_LINE = "\n\r";
+    public static final String HOST = "192.168.1.104";
+    //public static final String HOST = "194.87.235.135";
     public static final int PORT = 8080;
+
+
 }
