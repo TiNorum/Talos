4af703a52baed709730cf3f0a482d539246b899a
GTai <43651069+talosgtai@users.noreply.github.com>
2020-05-05 16:47:20 +0300

    Изменения: - Обновление дизайна

--- app/src/main/java/com/example/myapplication/Instruments/ShowTost.java
+++ app/src/main/java/com/example/myapplication/Instruments/ShowTost.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
 import android.view.Gravity;
 import android.widget.Toast;
@@ -16,7 +17,7 @@ public class ShowTost {
         spannable.setSpan(new ForegroundColorSpan(Color.RED), 0, text.length(),  Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         Toast toast = Toast.makeText(context, spannable, Toast.LENGTH_SHORT);
-
+        // сделать setview для тоаста
 
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
